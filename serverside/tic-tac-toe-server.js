e7e8074f778055944aed971510593d8a43345be7
giladyavneh <67857522+giladyavneh@users.noreply.github.com>
2020-08-19 19:24:36 +0300

    post now returns the whole list

--- serverside/tic-tac-toe-server.js
+++ serverside/tic-tac-toe-server.js
@@ -11,7 +11,7 @@ app.get("/api/v1/records",(req,res)=>{
 app.post("/api/v1/records",(req,res)=>{
     req.body.id=records.length===0?1:Math.max(...records.map(record=>record.id))+1
     records.push(req.body);
-    res.send(req.body);
+    res.send(records);
 })
 
 app.listen(4000)
