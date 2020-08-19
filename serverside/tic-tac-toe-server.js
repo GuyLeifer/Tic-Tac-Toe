0ad41576c3c40f8394e408ae36baa1c5fbe3fb2d
giladyavneh <67857522+giladyavneh@users.noreply.github.com>
2020-08-19 21:51:18 +0300

    added a memory file

--- serverside/tic-tac-toe-server.js
+++ serverside/tic-tac-toe-server.js
@@ -1,10 +1,10 @@
 const fs=require("fs")
-console.log(fs.readFileSync("score.json","utf-8"))
+
 const express = require("express")
 const app = express()
 app.use(express.json())
 // app.use(express.static(".//build"))
-const records=[];
+const records=JSON.parse(fs.readFileSync("scores.json"));
 
 app.get("/api/v1/records",(req,res)=>{
     res.send(records)
@@ -13,7 +13,9 @@ app.get("/api/v1/records",(req,res)=>{
 app.post("/api/v1/records",(req,res)=>{
     req.body.id=records.length===0?1:Math.max(...records.map(record=>record.id))+1
     records.push(req.body);
-    
+    let history=JSON.parse(fs.readFileSync("scores.json"))
+    history.push(req.body)
+    fs.writeFileSync("scores.json",JSON.stringify(history))
     res.send(records);
 })
 
