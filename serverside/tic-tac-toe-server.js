ebdd214cd64efc683e17fe24c3c5ad5294fe8e92
giladyavneh <67857522+giladyavneh@users.noreply.github.com>
2020-08-19 17:27:36 +0300

    add "proxy", created a server
    listening for now on port 4000

---
+++ serverside/tic-tac-toe-server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+app.use(express.json())
+
+const records=[];
+
+app.get("/api/v1/records",(req,res)=>{
+    res.send(records)
+})
+
+app.post("/api/v1/records",(req,res)=>{
+    records.push(req.body);
+    res.send(req.body);
+})
+
+app.listen(4000)
