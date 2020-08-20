a060db51a7d321430f2582e91f3daead4d763b28
GuyLeifer <67858228+GuyLeifer@users.noreply.github.com>
2020-08-20 10:18:22 +0300

    add organization

--- serverside/tic-tac-toe-server.js
+++ serverside/tic-tac-toe-server.js
@@ -1,22 +1,22 @@
-const fs=require("fs")
+const fs = require("fs");
+const express = require("express");
 
-const express = require("express")
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 // app.use(express.static(".//build"))
-const records=JSON.parse(fs.readFileSync("scores.json"));
+const records = JSON.parse(fs.readFileSync("scores.json"));
 
-app.get("/api/v1/records",(req,res)=>{
+app.get("/api/v1/records", (req,res) => {
     res.send(records)
-})
+});
 
-app.post("/api/v1/records",(req,res)=>{
-    req.body.id=records.length===0?1:Math.max(...records.map(record=>record.id))+1
+app.post("/api/v1/records", (req,res) => {
+    req.body.id = records.length === 0 ? 1 : Math.max(...records.map(record => record.id)) + 1;
     records.push(req.body);
-    let history=JSON.parse(fs.readFileSync("scores.json"))
-    history.push(req.body)
-    fs.writeFileSync("scores.json",JSON.stringify(history))
+    let history = JSON.parse(fs.readFileSync("scores.json"));
+    history.push(req.body);
+    fs.writeFileSync("scores.json", JSON.stringify(history));
     res.send(records);
-})
+});
 
-app.listen(4000)
+app.listen(4000);
